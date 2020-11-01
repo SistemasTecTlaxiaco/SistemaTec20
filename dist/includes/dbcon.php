6de69c68ac4e5be6d63989d0f76adf54ecb84aee
IsraelMerlyn <merlyn142a@gmail.com>
2020-11-01 00:45:42 -0600

    sistemafinal

---
+++ dist/includes/dbcon.php
@@ -0,0 +1,12 @@
+<?php
+$con = mysqli_connect("localhost","root","","examen_online");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+date_default_timezone_set("America/Mexico_City"); 
+?>
+
