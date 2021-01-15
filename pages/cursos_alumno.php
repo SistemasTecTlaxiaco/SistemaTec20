cf270807aa593cc8a111fd22fb001b4a2841916a
omarlopz <aokijump20@gmail.com>
2021-01-15 11:04:04 -0600

    Asignacion de fuente a la pagina de cursos_alumnos

---
+++ pages/cursos_alumno.php
@@ -0,0 +1,34 @@
+
+<?php include 'header.php';
+
+
+?>
+
+    <!-- Empezamos con la fuente -->
+    <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="../plugins/select2/select2.min.css">
+    <!-- Finaliza. -->
+    <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <?php include 'main_sidebar.php';?>
+
+        <!-- navegacion superior -->
+       <?php include 'top_nav.php';?>      <!-- /fin de navegacion -->
+       <style>
+label{
+
+color: black;
+}
+li {
+  color: white;
+}
+ul {
+  color: white;
+}
+#buscar{
+  text-align: right;
+}
+       </style>
