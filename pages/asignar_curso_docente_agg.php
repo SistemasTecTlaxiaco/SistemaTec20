283be865099f8b4863e79ab5b0ec59628e38d0f8
EspeSanty <osoriosanty98@gmail.com>
2021-01-18 10:46:04 -0600

    guardado de curso

---
+++ pages/asignar_curso_docente_agg.php
@@ -0,0 +1,54 @@
+<?php include 'header.php';
+
+
+?>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="../plugins/select2/select2.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <?php include 'main_sidebar.php';?>
+
+        <!-- top navigation -->
+       <?php include 'top_nav.php';?>      <!-- /top navigation -->
+       <style>
+label{
+
+color: black;
+}
+li {
+  color: white;
+}
+ul {
+  color: white;
+}
+#buscar{
+  text-align: right;
+}
+       </style>
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class = "x-panel">
+
+            </div>
+
+        </div><!--end of modal-dialog-->
+ </div>
+
+
+                 <div class="panel-heading">
+
+
+        </div>
+ 
+
+                            <?php
