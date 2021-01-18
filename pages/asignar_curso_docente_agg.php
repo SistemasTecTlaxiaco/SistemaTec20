4550cf4f7f1ab2670b4a96c7403e57be2400feb4
IsraelMerlyn <merlyn142a@gmail.com>
2021-01-18 11:40:51 -0600

    AsignandoCursoDocente

--- pages/asignar_curso_docente_agg.php
+++ pages/asignar_curso_docente_agg.php
@@ -123,40 +123,70 @@ $queryc=mysqli_query($con,"select * from temporada   ")or die(mysqli_error());
    
 <button type="submit" class="btn btn-primary">Guardar cambios</button>
 
-
-
-
-
-
-
-
-
-
-
 <div class="modal-footer">
-
 
 </div>
 </form>
 
 
-
-
-
-  </div><!-- /.box-body -->
-
+</div><!-- /.box-body -->
 
 </div><!-- /.col -->
 
 
 </div><!-- /.row -->
 
-
-
-
   </div><!-- /.box-body -->
+</div>
+</div>
+</div>
+</div>
 
-</div>
-</div>
-</div>
-</div>
+<!-- footer content -->
+<footer>
+          <div class="pull-right">
+            Sistema de Examen online <a href="#"></a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+  <?php include 'datatable_script.php';?>
+
+
+
+        <script>
+        $(document).ready( function() {
+                $('#example2').dataTable( {
+                 "language": {
+                   "paginate": {
+                      "previous": "anterior",
+                      "next": "posterior"
+                    },
+                    "search": "Buscar:",
+
+
+                  },
+
+                  "info": false,
+                  "lengthChange": false,
+                  "searching": false,
+
+
+  "searching": true,
+                }
+
+              );
+              } );
+    </script>
+
+
+
+
+
+
+    <!-- /gauge.js -->
+  </body>
+</html>
