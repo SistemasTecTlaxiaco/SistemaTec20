15f75356e8cc88409a245a94bbf6b79fb3523a2b
omarlopz <aokijump20@gmail.com>
2021-01-17 18:34:02 -0600

    Encabezado y fuente de curso docente

---
+++ pages/asignar_curso_docente.php
@@ -0,0 +1,33 @@
+<?php include 'header.php';
+
+
+?>
+
+    <!-- Agregando Fuene -->
+    <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="../plugins/select2/select2.min.css">
+    <!-- Continua -->
+    <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <?php include 'main_sidebar.php';?>
+
+        <!-- Encabezado -->
+       <?php include 'top_nav.php';?>      <!-- /encabezado-->
+       <style>
+label{
+
+color: black;
+}
+li {
+  color: white;
+}
+ul {
+  color: white;
+}
+#buscar{
+  text-align: right;
+}
+       </style>
