254ec0be55561fdbd6ca0697df99b16ff91a5633
claudiasilvavenda <61942126+claudiasilvavenda@users.noreply.github.com>
2021-01-19 16:26:44 -0600

    CreandoClaseExamen

---
+++ pages/examen.php
@@ -0,0 +1,20 @@
+
+<?php include 'header.php';
+
+
+?>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="../plugins/select2/select2.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <?php include 'main_sidebar.php';?>
+
+        <!-- top navigation -->
+       <?php include 'top_nav.php';?>      <!-- /top navigation -->
