1872490941e4e5ad9ec7c182cea5731d50f7d87a
Elvis <72048969+ElvisS10@users.noreply.github.com>
2021-07-04 01:42:46 -0500

    Quitamos las variabls DB_

--- pages/php/config.php
+++ pages/php/config.php
@@ -1,7 +1,8 @@
 <?php
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'tramite');
-	define('DB_CHARSET', 'utf8');
+	//
+	define('HOST', 'localhost');
+	define('USER', 'root');
+	define('PASS', '');
+	define('NAME', 'tramite');
+	define('CHARSET', 'utf8');
 ?>
